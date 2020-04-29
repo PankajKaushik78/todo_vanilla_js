d46bf20bcc18f4f42271894beb1ef41ecf7a371b
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2020-04-29 22:18:48 +0530

    Configured add todo

--- app.js
+++ app.js
@@ -15,8 +15,24 @@ function addTodo(event) {
     //Todo Div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
-
     //creating a Todo
     const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class='fas fa-check'></i>";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
 
+    //Adding to main list
+    todoList.appendChild(todoDiv);
+    //Clearing the input
+    todoInput.value = '';
 }
